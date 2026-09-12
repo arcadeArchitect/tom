cf257c84950d115fe5876f22b5dfa2e8a5235382
James <jlguleno@gmail.com>
2026-09-12 01:47:29 -0700

    separate embed.py into classes

--- src/tom/embed.py
+++ src/tom/embed.py
@@ -1,25 +1,38 @@
 import json, hashlib
 from pathlib import Path
 
+import torch
+import torch.nn as nn
 
-SPECIALS = ["<pad>", "<unk>", "<bos>", "<eos>"]
-SPECIALS_MAPPING = {"pad": 0, "unk": 1, "bos": 2, "eos": 3}
 
-# returns itos given input training text
-def build_vocab(text: str) -> list[str]:
-    return SPECIALS + sorted(set(text))
+class Vocab():
+    def __init__(self):
+        self.specials = ["<pad>", "<unk>", "<bos>", "<eos>"]
+        self.specials_mapping = {"pad": 0, "unk": 1, "bos": 2, "eos": 3}
 
-def vocab_hash(itos: list[str]) -> str:
-    encoded_string = json.dumps({"version": 1, "itos" : itos}, ensure_ascii=True, sort_keys=True, separators=(",", ":")).encode("utf-8")
-    return "sha256:" + hashlib.sha256(encoded_string).hexdigest()[:16]
+    # returns itos given input training text
+    def build_vocab(self, text: str) -> list[str]:
+        return self.specials + sorted(set(text))
 
-def save_vocab(itos: list, path: Path):
-    data = {"version": 1, "itos" : itos, "specials": SPECIALS_MAPPING, "hash": vocab_hash(itos)}
-    with open(path, "w", encoding="utf-8") as f:
-        json.dump(data, f, indent=4)
+    def vocab_hash(self, itos: list[str]) -> str:
+        encoded_string = json.dumps({"version": 1, "itos" : itos}, ensure_ascii=True, sort_keys=True, separators=(",", ":")).encode("utf-8")
+        return "sha256:" + hashlib.sha256(encoded_string).hexdigest()[:16]
 
-PROJECT_ROOT = Path(__file__).resolve().parents[2]
-DATA_DIR = PROJECT_ROOT / "data"
-DATA_DIR.mkdir(exist_ok=True)
+    def save_vocab(self, itos: list, path: Path):
+        data = {"version": 1, "itos" : itos, "specials": self.specials_mapping, "hash": self.vocab_hash(itos)}
+        with open(path, "w", encoding="utf-8") as f:
+            json.dump(data, f, indent=4)
 
-save_vocab(build_vocab("hi hi hi  hi a aaaaabcdefghiji@%#$639t73983wg-[p[p]]"), DATA_DIR / "test_vocab.json")
+    PROJECT_ROOT = Path(__file__).resolve().parents[2]
+    DATA_DIR = PROJECT_ROOT / "data"
+    DATA_DIR.mkdir(exist_ok=True)
+
+    save_vocab(build_vocab("hi hi hi  hi a aaaaabcdefghiji@%#$639t73983wg-[p[p]]"), DATA_DIR / "test_vocab.json")
+
+
+class Embedder(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self, x):
+        return None
