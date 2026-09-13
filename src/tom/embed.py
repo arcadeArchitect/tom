48b4180b31471374c3081614c8e282ba8180cf2e
James <jlguleno@gmail.com>
2026-09-13 10:54:30 -0700

    restructure vocab saving, move test call outside Vocab class

--- src/tom/embed.py
+++ src/tom/embed.py
@@ -18,16 +18,21 @@ class Vocab():
         encoded_string = json.dumps({"version": 1, "itos" : itos}, ensure_ascii=True, sort_keys=True, separators=(",", ":")).encode("utf-8")
         return "sha256:" + hashlib.sha256(encoded_string).hexdigest()[:16]
 
-    def save_vocab(self, itos: list, path: Path):
+    def save_vocab_file(self, itos: list, path: Path):
         data = {"version": 1, "itos" : itos, "specials": self.specials_mapping, "hash": self.vocab_hash(itos)}
         with open(path, "w", encoding="utf-8") as f:
             json.dump(data, f, indent=4)
+    
+    def save_vocab(self, text: str, file_name: str = "vocab.json", folder_name: str = "data"):
+        PROJECT_ROOT = Path(__file__).resolve().parents[2]
+        DATA_DIR = PROJECT_ROOT / folder_name
+        DATA_DIR.mkdir(exist_ok=True)
+        itos = self.build_vocab(text)
+        self.save_vocab_file(itos, DATA_DIR / file_name)
 
-    PROJECT_ROOT = Path(__file__).resolve().parents[2]
-    DATA_DIR = PROJECT_ROOT / "data"
-    DATA_DIR.mkdir(exist_ok=True)
 
-    save_vocab(build_vocab("hi hi hi  hi a aaaaabcdefghiji@%#$639t73983wg-[p[p]]"), DATA_DIR / "test_vocab.json")
+vocab = Vocab()
+vocab.save_vocab("hi hi hi  hi a aaaaabcdefghiji@%#$639t73983wg-[p[p]]π🥺", file_name="test_vocab.json")
 
 
 class Embedder(nn.Module):
